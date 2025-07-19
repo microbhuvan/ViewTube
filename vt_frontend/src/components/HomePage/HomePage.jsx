4efcd75869f296132e752f0d4a9cdc99fbc006f2
microbhuvan <microbhuvan@gmail.com>
2025-07-19 22:43:40 +0530

    implemented home page

--- vt_frontend/src/components/HomePage/HomePage.jsx
+++ vt_frontend/src/components/HomePage/HomePage.jsx
@@ -1,7 +1,197 @@
 import "./homepage.css";
 
-const HomePage = () => {
-  return <div>home page</div>;
+const HomePage = ({ sideNavbar }) => {
+  const options = [
+    "all",
+    "music",
+    "cricket",
+    "movies",
+    "all",
+    "music",
+    "cricket",
+    "movies",
+    "all",
+    "music",
+    "cricket",
+    "movies",
+    "all",
+    "music",
+    "cricket",
+    "movies",
+    "all",
+    "music",
+    "cricket",
+    "movies",
+  ];
+  return (
+    <div className={sideNavbar ? "homePage" : "homePageFull"}>
+      {/**homepage scroll*/}
+      <div className="homePageOptions">
+        {options.map((option, index) => (
+          <div key={index} className="homePageOptionElements">
+            {option}
+          </div>
+        ))}
+      </div>
+
+      {/**homepage MAIN*/}
+      <div className="homeMainPage">
+        <div className="vt-videoContainer">
+          <div className="vt-video">
+            <div className="vt-thumbnailBox">
+              <img
+                src="https://cdn.neowin.com/news/images/uploaded/2024/08/1723555868_youtube-logo.jpg"
+                alt="thumbnail"
+                className="vt-thumbnail"
+              />
+              <div className="vt-thumbnailTime">18:50</div>
+            </div>
+            <div className="vt-titleBox">
+              <div className="vt-titleBoxProfile">
+                <img
+                  src="https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+                  alt="profile link"
+                  className="vt-titleBoxProflePic"
+                />
+              </div>
+
+              <div className="vt-titleBoxTitle">
+                <div className="vt-videoTitle">smurfs-movie review</div>
+                <div className="vt-videoChannelName">jeremy jahans</div>
+                <div className="vt-videoViews">3 views</div>
+              </div>
+            </div>
+          </div>
+          {/** */}
+          <div className="vt-video">
+            <div className="vt-thumbnailBox">
+              <img
+                src="https://cdn.neowin.com/news/images/uploaded/2024/08/1723555868_youtube-logo.jpg"
+                alt="thumbnail"
+                className="vt-thumbnail"
+              />
+              <div className="vt-thumbnailTime">18:50</div>
+            </div>
+            <div className="vt-titleBox">
+              <div className="vt-titleBoxProfile">
+                <img
+                  src="https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+                  alt="profile link"
+                  className="vt-titleBoxProflePic"
+                />
+              </div>
+
+              <div className="vt-titleBoxTitle">
+                <div className="vt-videoTitle">smurfs-movie review</div>
+                <div className="vt-videoChannelName">jeremy jahans</div>
+                <div className="vt-videoViews">3 views</div>
+              </div>
+            </div>
+          </div>
+          <div className="vt-video">
+            <div className="vt-thumbnailBox">
+              <img
+                src="https://cdn.neowin.com/news/images/uploaded/2024/08/1723555868_youtube-logo.jpg"
+                alt="thumbnail"
+                className="vt-thumbnail"
+              />
+              <div className="vt-thumbnailTime">18:50</div>
+            </div>
+            <div className="vt-titleBox">
+              <div className="vt-titleBoxProfile">
+                <img
+                  src="https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+                  alt="profile link"
+                  className="vt-titleBoxProflePic"
+                />
+              </div>
+
+              <div className="vt-titleBoxTitle">
+                <div className="vt-videoTitle">smurfs-movie review</div>
+                <div className="vt-videoChannelName">jeremy jahans</div>
+                <div className="vt-videoViews">3 views</div>
+              </div>
+            </div>
+          </div>
+          <div className="vt-video">
+            <div className="vt-thumbnailBox">
+              <img
+                src="https://cdn.neowin.com/news/images/uploaded/2024/08/1723555868_youtube-logo.jpg"
+                alt="thumbnail"
+                className="vt-thumbnail"
+              />
+              <div className="vt-thumbnailTime">18:50</div>
+            </div>
+            <div className="vt-titleBox">
+              <div className="vt-titleBoxProfile">
+                <img
+                  src="https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+                  alt="profile link"
+                  className="vt-titleBoxProflePic"
+                />
+              </div>
+
+              <div className="vt-titleBoxTitle">
+                <div className="vt-videoTitle">smurfs-movie review</div>
+                <div className="vt-videoChannelName">jeremy jahans</div>
+                <div className="vt-videoViews">3 views</div>
+              </div>
+            </div>
+          </div>
+          <div className="vt-video">
+            <div className="vt-thumbnailBox">
+              <img
+                src="https://cdn.neowin.com/news/images/uploaded/2024/08/1723555868_youtube-logo.jpg"
+                alt="thumbnail"
+                className="vt-thumbnail"
+              />
+              <div className="vt-thumbnailTime">18:50</div>
+            </div>
+            <div className="vt-titleBox">
+              <div className="vt-titleBoxProfile">
+                <img
+                  src="https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+                  alt="profile link"
+                  className="vt-titleBoxProflePic"
+                />
+              </div>
+
+              <div className="vt-titleBoxTitle">
+                <div className="vt-videoTitle">smurfs-movie review</div>
+                <div className="vt-videoChannelName">jeremy jahans</div>
+                <div className="vt-videoViews">3 views</div>
+              </div>
+            </div>
+          </div>
+          <div className="vt-video">
+            <div className="vt-thumbnailBox">
+              <img
+                src="https://cdn.neowin.com/news/images/uploaded/2024/08/1723555868_youtube-logo.jpg"
+                alt="thumbnail"
+                className="vt-thumbnail"
+              />
+              <div className="vt-thumbnailTime">18:50</div>
+            </div>
+            <div className="vt-titleBox">
+              <div className="vt-titleBoxProfile">
+                <img
+                  src="https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+                  alt="profile link"
+                  className="vt-titleBoxProflePic"
+                />
+              </div>
+
+              <div className="vt-titleBoxTitle">
+                <div className="vt-videoTitle">smurfs-movie review</div>
+                <div className="vt-videoChannelName">jeremy jahans</div>
+                <div className="vt-videoViews">3 views</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
