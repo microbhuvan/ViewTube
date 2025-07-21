88ec78e38bbc84eefd51411276971f7ba5ba5453
microbhuvan <microbhuvan@gmail.com>
2025-07-21 16:15:46 +0530

    configuration with cloudinary and file uploads

--- vt_frontend/src/Pages/SignUp/SignUp.jsx
+++ vt_frontend/src/Pages/SignUp/SignUp.jsx
@@ -1,24 +1,50 @@
 import "./signup.css";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
 
 const SignUp = () => {
+  const [uploadImageUrl, setUploadImageUrl] = useState(
+    "https://media.istockphoto.com/id/1131164548/vector/avatar-5.jpg?s=612x612&w=0&k=20&c=CK49ShLJwDxE4kiroCR42kimTuuhvuo2FH5y_6aSgEo="
+  );
   const [signUpField, setSignUpField] = useState({
     userName: "",
     password: "",
     emailId: "",
     about: "",
+    profilePic: uploadImageUrl,
   });
-
-  console.log(signUpField);
-
   const handleInputField = (event, name) => {
     setSignUpField({
       ...signUpField,
       [name]: event.target.value,
     });
   };
+
+  const uploadImage = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("folder", "viewtube");
+    //ViewTube
+    data.append("upload_preset", "ViewTube");
+    try {
+      const res = await axios.post(
+        `https://api.cloudinary.com/v1_1/${
+          import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+        }/image/upload`,
+        data
+      );
+      const imageUrl = res.data.url;
+      setUploadImageUrl(imageUrl);
+      setSignUpField({ ...signUpField, profilePic: imageUrl });
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  console.log(signUpField);
 
   return (
     <div className="signup">
@@ -71,8 +97,14 @@ const SignUp = () => {
             <input
               type="file"
               accept="image/*"
+              onChange={(e) => uploadImage(e)}
               className="inputButtons"
             ></input>
+            <img
+              src={uploadImageUrl}
+              alt="profile pic"
+              className="profileImage"
+            ></img>
           </div>
         </div>
         <div className="signupButtons">
--- vt_frontend/src/Pages/VideoUpload/VideoUpload.jsx
+++ vt_frontend/src/Pages/VideoUpload/VideoUpload.jsx
@@ -1,6 +1,9 @@
 import "./videoupload.css";
 import { useState } from "react";
+import axios from "axios";
 import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import * as React from "react";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const VideoUpload = () => {
   const [inputField, setInputField] = useState({
@@ -8,14 +11,44 @@ const VideoUpload = () => {
     description: "",
     category: "",
     thumbnail: "",
-    video: "",
+    videoLink: "",
   });
 
   console.log(inputField);
-
+  const [loader, setLoader] = useState(false);
   const handleOnChangeInput = (event, name) => {
     setInputField({ ...inputField, [name]: event.target.value });
   };
+
+  const uploadField = async (e, type) => {
+    setLoader(true);
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("folder", "viewtube");
+    //ViewTube
+    data.append("upload_preset", "ViewTube");
+    try {
+      const res = await axios.post(
+        `https://api.cloudinary.com/v1_1/${
+          import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
+        }/${type}/upload`,
+        data
+      );
+      setLoader(false);
+      const url = res.data.url;
+      let val = type === "image" ? "thumbnail" : "videoLink";
+      setInputField({ ...inputField, [val]: url });
+
+      console.log(res);
+    } catch (err) {
+      setLoader(false);
+      console.log(err);
+    }
+  };
+
+  console.log(inputField);
+
   return (
     <div className="videoUpload">
       <div className="uploadBox">
@@ -59,10 +92,7 @@ const VideoUpload = () => {
               type="file"
               accept="image/*"
               className="inputButtons"
-              value={inputField.thumbnail}
-              onChange={(e) => {
-                handleOnChangeInput(e, "thumbnail");
-              }}
+              onChange={(e) => uploadField(e, "image")}
             ></input>
           </div>
           <div className="uploadBtnTV">
@@ -71,11 +101,9 @@ const VideoUpload = () => {
               type="file"
               accept="video/mp4, video/webm, video/*"
               className="inputButtons"
-              value={inputField.video}
-              onChange={(e) => {
-                handleOnChangeInput(e, "video");
-              }}
+              onChange={(e) => uploadField(e, "video")}
             ></input>
+            {loader && <CircularProgress disableShrink />}
           </div>
         </div>
         <div className="uploadBtns">
